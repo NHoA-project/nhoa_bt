8cba33d5ff702780f5b599baba9159deecca49b2
oscar.palacin <oscar.palacin@eurecat.org>
2022-12-01 17:20:52 +0100

    Necessary files to export GI outside the docker image.

---
+++ nhoa_bt/.xauth_docker/config/set_xauth_for_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+HOME_PATH="${HOME}" #Modify with your home directory.
+DAUTH_PATH=$HOME_PATH/docker/.xauth_docker
+XAUTH=$DAUTH_PATH/share/.docker.xauth 
+if [ -f $XAUTH ]
+then
+  rm $XAUTH    
+fi 
+if [ ! -f $XAUTH ]
+then
+    xauth nlist $DISPLAY | sed -e 's/^..../ffff/' > $DAUTH_PATH/tempAuth.txt   	
+    xauth -f $XAUTH nmerge $DAUTH_PATH/tempAuth.txt        
+    rm $DAUTH_PATH/tempAuth.txt        
+    chmod a+r $XAUTH
+fi
