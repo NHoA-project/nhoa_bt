94dd417f8a44ee95fdaf0348133853ce9d9993bf
oscar.palacin <oscar.palacin@eurecat.org>
2022-12-14 18:36:36 +0100

    UPO and UOC conditions added.

--- nhoa_bt/src/bt_condition_nodes/IsUserDetected.cpp
+++ nhoa_bt/src/bt_condition_nodes/IsUserDetected.cpp
@@ -2,58 +2,58 @@
 #include  <iostream>
 
 BT::NodeStatus IsUserDetected::tick() {
-    // TODO: HARDCODED VERSION.
-    std::string input;
-    while (true) {
-        std::cout << name() << ": Is user detected? Select 's' (success) or 'f'(failure)" << std::endl;
-        std::cin >> input;
-        if (input.compare("s") == 0) 
-        { 
-            std::cout << "User is detected!" << std::endl;
-            return BT::NodeStatus::SUCCESS;
-        } 
-        else if(input.compare("f") == 0) 
-        {
-            std::cout << "User is NOT detected!" << std::endl;
-            return BT::NodeStatus::FAILURE;
-        } 
-    }
-
-    // // TODO: CLEAN VERSION.
+    // // TODO: HARDCODED VERSION.
     // std::string input;
-    // bool waiting = true;
-    // bool success = false;
-    // while (waiting) 
-    // {
-    //     std::cout << name() << ": Select 's' (start) or 'p'(pause)" << std::endl;
+    // while (true) {
+    //     std::cout << name() << ": Is user detected? Select 's' (success) or 'f'(failure)" << std::endl;
     //     std::cin >> input;
     //     if (input.compare("s") == 0) 
     //     { 
-    //         if(isUserDetected())
-    //             success = true;
-
-    //         waiting = false;
+    //         std::cout << "User is detected!" << std::endl;
+    //         return BT::NodeStatus::SUCCESS;
+    //     } 
+    //     else if(input.compare("f") == 0) 
+    //     {
+    //         std::cout << "User is NOT detected!" << std::endl;
+    //         return BT::NodeStatus::FAILURE;
     //     } 
     // }
 
-    // if(success)
-    // {
-    //     return BT::NodeStatus::SUCCESS;
-    // }
-    // else
-    // {
-    //     return BT::NodeStatus::FAILURE;
-    // }
+    // TODO: CLEAN VERSION.
+    std::string input;
+    bool waiting = true;
+    bool success = false;
+    while (waiting) 
+    {
+        std::cout << name() << ": Select 's' (start) or 'p'(pause)" << std::endl;
+        std::cin >> input;
+        if (input.compare("s") == 0) 
+        { 
+            if(isUserDetected())
+                success = true;
+
+            waiting = false;
+        } 
+    }
+
+    if(success)
+    {
+        return BT::NodeStatus::SUCCESS;
+    }
+    else
+    {
+        return BT::NodeStatus::FAILURE;
+    }
 }
 
 // ####################
 // Additional functions.
 bool IsUserDetected::isUserDetected()
 {
-    if( !(hri_->body_id_.empty()) || 
-        !(hri_->face_id_.empty()) || 
-        !(hri_->person_id_.empty()) ) // TODO: HRI ID detection
-    // if( hri_->engagement_level_.compare("engaged") == 0 ) // TODO: EngagementLevel
+//     if( !(hri_->body_id_.empty()) || 
+//         !(hri_->face_id_.empty()) || 
+//         !(hri_->person_id_.empty()) ) // TODO: HRI ID detection
+    if(head_motion_->head_following_feedback_.feedback.found)
     {
         success_ = true;
     }
--- nhoa_bt/src/bt_condition_nodes/IsUserEngaged.cpp
+++ nhoa_bt/src/bt_condition_nodes/IsUserEngaged.cpp
@@ -3,73 +3,66 @@
 
 BT::NodeStatus IsUserEngaged::tick() {
     // TODO: HARDCODED VERSION.
-    std::string input;
-    while (true) 
-    {
-        std::cout << name() << ": Select user engagement level '0' (UNKNOWN), '1' (DISENGAGED), '2' (ENGAGING), '3' (ENGAGED), '4' (DISENGAGING)" << std::endl;
-        std::cin >> input;
-        if (input.compare("0") == 0) 
-        { 
-            std::cout << "Engagement Level -> '0' (UNKNOWN)" << std::endl;
-            return BT::NodeStatus::FAILURE;
-        } 
-        else if (input.compare("1") == 0) 
-        { 
-            std::cout << "Engagement Level -> '1' (DISENGAGED)" << std::endl;
-            return BT::NodeStatus::FAILURE;
-        } 
-        else if (input.compare("2") == 0) 
-        { 
-            std::cout << "Engagement Level -> '2' (ENGAGING)" << std::endl;
-            return BT::NodeStatus::FAILURE;
-        } 
-        else if (input.compare("3") == 0) 
-        { 
-            std::cout << "Engagement Level -> '3' (ENGAGED)" << std::endl;
-            return BT::NodeStatus::SUCCESS;
-        } 
-        else if (input.compare("4") == 0) 
-        { 
-            std::cout << "Engagement Level -> '4' (DISENGAGING)" << std::endl;
-            return BT::NodeStatus::FAILURE;
-        } 
-    }
-
-    // // TODO: CLEAN VERSION.
     // std::string input;
-    // bool waiting = true;
-    // bool success = false;
-    // while (waiting) 
+    // while (true) 
     // {
-    //     std::cout << name() << ": Select 's' (start) or 'p'(pause)" << std::endl;
+    //     std::cout << name() << ": Select user engagement level '0' (UNKNOWN), '1' (DISENGAGED), '2' (ENGAGING), '3' (ENGAGED), '4' (DISENGAGING)" << std::endl;
     //     std::cin >> input;
-    //     if (input.compare("s") == 0) 
+    //     if (input.compare("0") == 0) 
     //     { 
-    //         if(isUserEngaged())
-    //             success = true;
-
-    //         waiting = false;
+    //         std::cout << "Engagement Level -> '0' (UNKNOWN)" << std::endl;
+    //         return BT::NodeStatus::FAILURE;
+    //     } 
+    //     else if (input.compare("1") == 0) 
+    //     { 
+    //         std::cout << "Engagement Level -> '1' (DISENGAGED)" << std::endl;
+    //         return BT::NodeStatus::FAILURE;
+    //     } 
+    //     else if (input.compare("2") == 0) 
+    //     { 
+    //         std::cout << "Engagement Level -> '2' (ENGAGING)" << std::endl;
+    //         return BT::NodeStatus::FAILURE;
+    //     } 
+    //     else if (input.compare("3") == 0) 
+    //     { 
+    //         std::cout << "Engagement Level -> '3' (ENGAGED)" << std::endl;
+    //         return BT::NodeStatus::SUCCESS;
+    //     } 
+    //     else if (input.compare("4") == 0) 
+    //     { 
+    //         std::cout << "Engagement Level -> '4' (DISENGAGING)" << std::endl;
+    //         return BT::NodeStatus::FAILURE;
     //     } 
     // }
 
-    // if(success)
-    // {
-    //     return BT::NodeStatus::SUCCESS;
-    // }
-    // else
-    // {
-    //     return BT::NodeStatus::FAILURE;
-    // }
+    // TODO: ROS4HRI CLEAN VERSION.
+    std::string input;
+    bool waiting = true;
+    bool success = false;
+    while (waiting) 
+    {
+        std::cout << name() << ": Select 's' (start) or 'p'(pause)" << std::endl;
+        std::cin >> input;
+        if (input.compare("s") == 0) 
+        { 
+            if(isUserEngaged())
+                success = true;
+
+            waiting = false;
+        } 
+    }
 }
 
 // ####################
 // Additional functions.
 bool IsUserEngaged::isUserEngaged()
 {
-    if( hri_->smile_score_ > hri_->smile_score_threshold_ ) // TODO: SmileScore
+    if( hri_->engagement_level_acum_ > hri_->engaged_level_threshold_)
+    // if( hri_->smile_score_ > hri_->smile_score_threshold_ ) // TODO: SmileScore
     // if( hri_->engagement_level_.compare("engaged") == 0 ) // TODO: EngagementLevel
     {
         success_ = true;
+        hri_->engagement_level_acum_ = 0;
     }
     else
     {
