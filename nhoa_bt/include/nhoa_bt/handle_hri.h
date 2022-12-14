99294d5184d8edcdf92faf9ebec47a1778c21d6d
oscar.palacin <oscar.palacin@eurecat.org>
2022-12-14 18:39:17 +0100

    Comment deprecated functionalities and add UOC new ones.

--- nhoa_bt/include/nhoa_bt/handle_hri.h
+++ nhoa_bt/include/nhoa_bt/handle_hri.h
@@ -5,6 +5,8 @@
 #include <exception>
 #include <string>
 #include "std_msgs/Float32.h"
+#include "std_msgs/Int32.h"
+#include "std_msgs/Int32MultiArray.h"
 
 // Boost headers
 #include <boost/shared_ptr.hpp>
@@ -35,13 +37,17 @@ private:
 
     // HRI stuff.
     // - ROS subscribers.
-    ros::Subscriber     candidate_matches_sub_; 
-    ros::Subscriber     engagement_level_sub_;
+    // ros::Subscriber     candidate_matches_sub_; 
+    // ros::Subscriber     engagement_level_sub_;
     ros::Subscriber     live_speech_sub_;
-    ros::Subscriber     smile_score_sub_;       // UOC
-    ros::Subscriber     tracked_bodies_sub_;
-    ros::Subscriber     tracked_faces_sub_;  
-    ros::Subscriber     tracked_persons_sub_;
+    // ros::Subscriber     tracked_bodies_sub_;
+    // ros::Subscriber     tracked_faces_sub_;  
+    // ros::Subscriber     tracked_persons_sub_;
+
+    // UOC stuff.
+    ros::Subscriber     head_direction_sub_;
+    ros::Subscriber     head_direction_vector_sub_;
+    ros::Subscriber     smile_score_sub_;
 
     // - Booleans.
     bool                is_body_id_modified_    = false;
@@ -57,10 +63,16 @@ private:
     // =======
     //functions
     // Candidate matches callback.
-    void candidates_matches_callback(const hri_msgs::IdsMatch&  ids_match);
+    // void candidates_matches_callback(const hri_msgs::IdsMatch&  ids_match);
 
     // Person engagement level callback.
-    void engagement_level_callback(const hri_msgs::EngagementLevel&  engagement);
+    // void engagement_level_callback(const hri_msgs::EngagementLevel&  engagement);
+
+    // Head direction callback.
+    void head_direction_callback(const std_msgs::Int32ConstPtr&  direction);
+
+    // Head direction vector callback.
+    void head_direction_vector_callback(const std_msgs::Int32MultiArrayConstPtr&  vector);
 
     // Initializing.
     void init();
@@ -70,51 +82,61 @@ private:
 
     // Matched ID subscribers' loop. It checks the current face/body/person
     // ID and launches the subscribers.
-    void matched_id_subscriber_loop();
+    // void matched_id_subscriber_loop();
 
     // Set engagement level.
-    void set_engagement_level(const uint &level);
+    // void set_engagement_level(const uint &level);
 
     // Set matched ID.
-    void set_id_matches(const std::string &id,
-                        const std::string &id_type);
+    // void set_id_matches(const std::string &id,
+    //                     const std::string &id_type);
 
     // Smile score callback.
     void smile_score_callback(const std_msgs::Float32::ConstPtr&  score);
 
-    // Set ID type.
-    std::string set_id_type(const int   &id_type);
+    // // Set ID type.
+    // std::string set_id_type(const int   &id_type);
 
-    // Tracked persons callback.
-    void tracked_bodies_callback(const hri_msgs::IdsList&  ids_list);
+    // // Tracked persons callback.
+    // void tracked_bodies_callback(const hri_msgs::IdsList&  ids_list);
 
-    // Tracked persons callback.
-    void tracked_faces_callback(const hri_msgs::IdsList&  ids_list);
+    // // Tracked persons callback.
+    // void tracked_faces_callback(const hri_msgs::IdsList&  ids_list);
 
-    // Tracked persons callback.
-    void tracked_persons_callback(const hri_msgs::IdsList&  ids_list);
+    // // Tracked persons callback.
+    // void tracked_persons_callback(const hri_msgs::IdsList&  ids_list);
 
 
 public:
     //vars
-    std::string     body_id_;
-    std::string     engagement_level_;
-    std::string     face_id_;
-    std::string     id1_;
-    std::string     id2_;
-    std::string     id1_type_;
-    std::string     id2_type_;
-    std::string     person_id_;
-    std::string     speech_;
+    std::string         body_id_;
+    std::string         engagement_level_;
+    std::string         face_id_;
+    std::string         id1_;
+    std::string         id2_;
+    std::string         id1_type_;
+    std::string         id2_type_;
+    std::string         person_id_;
+    std::string         speech_;
 
-    bool            live_speech_flag_   = false;
+    bool                live_speech_flag_   = false;
 
-    double          match_confidence_   = 0.0;
-    double          smile_score_        = 0.0;
-    double          smile_score_threshold_;
+    double              match_confidence_   = 0.0;
+    double              smile_score_threshold_;
+    size_t              smile_acum_ = 0;
+    size_t              smile_calls_ = 0;
+
+    int                 engagement_level_acum_ = 0;
+    int                 engaged_level_threshold_;
+    int                 engaging_level_threshold_;
+    int                 head_direction_;
+    std::vector<int>    head_direction_vector_;
 
     // =======
     //functions
     handle_hri(ros::NodeHandle    *nodehandle);
+
+    // Set smile score.
+    double set_smile_score();
 };
 #endif // __handle_hri_H_INCLUDED__
--- nhoa_bt/src/handle_hri.cpp
+++ nhoa_bt/src/handle_hri.cpp
@@ -3,6 +3,9 @@
 // C++ standard headers
 #include <exception>
 #include <string>
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int32.h"
+#include "std_msgs/Int32MultiArray.h"
 
 // Boost headers
 #include <boost/shared_ptr.hpp>
@@ -29,37 +32,63 @@ handle_hri::handle_hri(ros::NodeHandle    *nodehandle):  nh_(*nodehandle)
 
 // ###################################
 
-void handle_hri::candidates_matches_callback(const hri_msgs::IdsMatch&  ids_match)
+// void handle_hri::candidates_matches_callback(const hri_msgs::IdsMatch&  ids_match)
+// {
+//   // Check id matches.
+//   // mutex_.lock();
+//   match_confidence_ = ids_match.confidence;
+//   id1_ = ids_match.id1;
+//   id1_type_ = handle_hri::set_id_type(ids_match.id1_type);
+//   id2_ = ids_match.id2;
+//   id2_type_ = handle_hri::set_id_type(ids_match.id2_type);
+//   // mutex_.unlock();
+// }
+
+// void handle_hri::engagement_level_callback(const hri_msgs::EngagementLevel&  engagement)
+// {
+//   handle_hri::set_engagement_level(engagement.level);
+// }
+
+void handle_hri::head_direction_callback(const std_msgs::Int32ConstPtr&  direction)
 {
-  // Check id matches.
-  // mutex_.lock();
-  match_confidence_ = ids_match.confidence;
-  id1_ = ids_match.id1;
-  id1_type_ = handle_hri::set_id_type(ids_match.id1_type);
-  id2_ = ids_match.id2;
-  id2_type_ = handle_hri::set_id_type(ids_match.id2_type);
-  // mutex_.unlock();
+  head_direction_ = direction->data;
+  std::cout << " ### Direction -> " << direction->data << std::endl; 
+  if(head_direction_ == 1)
+  {
+    engagement_level_acum_ += 1; 
+  }
+  std::cout << " ### Engagement level acum -> " << engagement_level_acum_ << std::endl;
 }
 
-void handle_hri::engagement_level_callback(const hri_msgs::EngagementLevel&  engagement)
+void handle_hri::head_direction_vector_callback(const std_msgs::Int32MultiArrayConstPtr&  vector)
 {
-  handle_hri::set_engagement_level(engagement.level);
+  head_direction_vector_[0] = vector->data[0];
+  head_direction_vector_[1] = vector->data[2];
+  head_direction_vector_[2] = vector->data[3];
 }
 
 void handle_hri::init()
 {
   std::cout << "Initializing handle_hri ..." << std::endl;
 
+  // Initialize variables.
+  engaged_level_threshold_    = nh_.param("engaged_level_threshold", 5);
+  engaging_level_threshold_   = nh_.param("engaging_level_threshold", 10);
+  // match_confidence_threshold_ = nh_.param("ids_match_confidence_threshold", 0.5);
+  smile_score_threshold_      = nh_.param("smile_score_threshold", 0.5);
+
+
   // Initialize user_input subscriber. 
-  candidate_matches_sub_    = nh_.subscribe("/humans/candidate_matches", 1, &handle_hri::candidates_matches_callback, this);
+  // candidate_matches_sub_    = nh_.subscribe("/humans/candidate_matches", 1, &handle_hri::candidates_matches_callback, this);
   live_speech_sub_          = nh_.subscribe("/humans/voices/anonymous_speaker/speech", 1, &handle_hri::live_speech_callback, this);
   // tracked_bodies_sub_       = nh_.subscribe("/humans/bodies/tracked", 1, &handle_hri::tracked_bodies_callback, this);
   // tracked_faces_sub_        = nh_.subscribe("/humans/faces/tracked", 1, &handle_hri::tracked_faces_callback, this);
   // tracked_persons_sub_      = nh_.subscribe("/humans/persons/tracked", 1, &handle_hri::tracked_persons_callback, this);
 
-  // Initialize variables.
-  match_confidence_threshold_ = nh_.param("ids_match_confidence_threshold", 0.5);
-  smile_score_threshold_      = nh_.param("smile_score_threshold", 0.5);
+  // UOC subscriber.
+  head_direction_sub_           = nh_.subscribe("/humans/faces/head_direction", 1, &handle_hri::head_direction_callback, this);
+  // head_direction_vector_sub_    = nh_.subscribe("/humans/faces/head_direction_vector", 1, &handle_hri::head_direction_vector_callback, this);
+  smile_score_sub_              = nh_.subscribe("/humans/faces/smile_score", 1, &handle_hri::smile_score_callback, this);
 
   std::cout << "handle_hri initialized!" << std::endl;
 }
@@ -72,126 +101,136 @@ void handle_hri::live_speech_callback(const hri_msgs::LiveSpeech&  speech)
     live_speech_flag_ = true;
 }
 
-void handle_hri::matched_id_subscriber_loop()
-{
-  while((body_id_.empty()) || (face_id_.empty()) || (person_id_.empty())) // TODO: Check that condition.
-  {
-    std::cout << "Tracked ID is empty." << std::endl;
-  }
+// void handle_hri::matched_id_subscriber_loop()
+// {
+//   while((body_id_.empty()) || (face_id_.empty()) || (person_id_.empty())) // TODO: Check that condition.
+//   {
+//     std::cout << "Tracked ID is empty." << std::endl;
+//   }
 
-  // Set the matched id (body, face and person).
-  handle_hri::set_id_matches(id1_, id1_type_);
-  handle_hri::set_id_matches(id2_, id2_type_);
+//   // Set the matched id (body, face and person).
+//   handle_hri::set_id_matches(id1_, id1_type_);
+//   handle_hri::set_id_matches(id2_, id2_type_);
 
-  engagement_level_sub_ = nh_.subscribe("/humans/person/" + person_id_ + "/engagement_status", 1, &handle_hri::engagement_level_callback, this);
-  smile_score_sub_      = nh_.subscribe("/humans/faces/" + face_id_ + "/smile_score", 1, &handle_hri::smile_score_callback, this);
+//   // engagement_level_sub_ = nh_.subscribe("/humans/person/" + person_id_ + "/engagement_status", 1, &handle_hri::engagement_level_callback, this);
+//   // smile_score_sub_      = nh_.subscribe("/humans/faces/" + face_id_ + "/smile_score", 1, &handle_hri::smile_score_callback, this);
 
-}
+// }
 
-void handle_hri::set_id_matches(const std::string &id,
-                                const std::string &id_type)
-{
-  if(match_confidence_ >= match_confidence_threshold_)
-  {
-    mutex_.lock();
-    if(id_type.compare("body") == 0)
-    {
-      body_id_ = id;
-    }
-    else if(id_type.compare("face") == 0)
-    {
-      face_id_ = id;
-    }
-    else if(id_type.compare("person") == 0)
-    {
-      person_id_ = id;
-    }
-    mutex_.unlock();
-  }
-}
+// void handle_hri::set_id_matches(const std::string &id,
+//                                 const std::string &id_type)
+// {
+//   if(match_confidence_ >= match_confidence_threshold_)
+//   {
+//     mutex_.lock();
+//     if(id_type.compare("body") == 0)
+//     {
+//       body_id_ = id;
+//     }
+//     else if(id_type.compare("face") == 0)
+//     {
+//       face_id_ = id;
+//     }
+//     else if(id_type.compare("person") == 0)
+//     {
+//       person_id_ = id;
+//     }
+//     mutex_.unlock();
+//   }
+// }
 
-void handle_hri::set_engagement_level(const uint &level)
-{
-  switch (level)
-  {
-  case 0:
-      engagement_level_ = "unknown";
-    break;
-  case 1:
-      engagement_level_ = "disengaged";
-    break;
-  case 2:
-      engagement_level_ = "engaging";
-    break;
-  case 3:
-      engagement_level_ = "engaged";
-    break;
-  case 4:
-      engagement_level_ = "disengaging";
-    break;
-  }
-}
+// void handle_hri::set_engagement_level(const uint &level)
+// {
+//   switch (level)
+//   {
+//   case 0:
+//       engagement_level_ = "unknown";
+//     break;
+//   case 1:
+//       engagement_level_ = "disengaged";
+//     break;
+//   case 2:
+//       engagement_level_ = "engaging";
+//     break;
+//   case 3:
+//       engagement_level_ = "engaged";
+//     break;
+//   case 4:
+//       engagement_level_ = "disengaging";
+//     break;
+//   }
+// }
 
-std::string handle_hri::set_id_type(const int &id_type)
-{
-  std::string type;
+// std::string handle_hri::set_id_type(const int &id_type)
+// {
+//   std::string type;
 
-  switch (id_type)
-  {
-  case 0:
-      type = "unset";
-    break;
-  case 1:
-      type = "person";
-    break;
-  case 2:
-      type = "face";
-    break;
-  case 3:
-      type = "body";
-    break;
-  case 4:
-      type = "voice";
-    break;
-  }
-  return type;
-}
+//   switch (id_type)
+//   {
+//   case 0:
+//       type = "unset";
+//     break;
+//   case 1:
+//       type = "person";
+//     break;
+//   case 2:
+//       type = "face";
+//     break;
+//   case 3:
+//       type = "body";
+//     break;
+//   case 4:
+//       type = "voice";
+//     break;
+//   }
+//   return type;
+// }
 
 void handle_hri::smile_score_callback(const std_msgs::Float32::ConstPtr&  score)
 {
   std::cout << "Smile score -> " << score->data << std::endl; 
-  smile_score_ = score->data;
+  // TODO: Valor ponderat de quantitat de vegades que s'ha superat el threshold.
+  if(score->data >= smile_score_threshold_)
+    smile_acum_ += 1;
+  smile_calls_ += 1;
+  std::cout << "Smile acum -> " << smile_acum_ << std::endl;
+  std::cout << "Smile calls -> " << smile_calls_ << std::endl;
 }
 
 
-void handle_hri::tracked_bodies_callback(const hri_msgs::IdsList&  ids_list)
-{
-  // Debug.
-  std::cout << "Body User ID -> " << ids_list.ids[0] << std::endl; 
-  if( !(body_id_.compare(ids_list.ids[0]) == 0) )
-  {
-    body_id_ = ids_list.ids[0]; 
-  }
-}
+// void handle_hri::tracked_bodies_callback(const hri_msgs::IdsList&  ids_list)
+// {
+//   // Debug.
+//   std::cout << "Body User ID -> " << ids_list.ids[0] << std::endl; 
+//   if( !(body_id_.compare(ids_list.ids[0]) == 0) )
+//   {
+//     body_id_ = ids_list.ids[0]; 
+//   }
+// }
 
-void handle_hri::tracked_faces_callback(const hri_msgs::IdsList&  ids_list)
-{
-  // Debug.
-  std::cout << "Face User ID -> " << ids_list.ids[0] << std::endl; 
-  if( !(face_id_.compare(ids_list.ids[0]) == 0) )
-  {
-    face_id_ = ids_list.ids[0]; 
-    handle_hri::matched_id_subscriber_loop();
-  }
-}
+// void handle_hri::tracked_faces_callback(const hri_msgs::IdsList&  ids_list)
+// {
+//   // Debug.
+//   std::cout << "Face User ID -> " << ids_list.ids[0] << std::endl; 
+//   if( !(face_id_.compare(ids_list.ids[0]) == 0) )
+//   {
+//     face_id_ = ids_list.ids[0]; 
+//     handle_hri::matched_id_subscriber_loop();
+//   }
+// }
 
-void handle_hri::tracked_persons_callback(const hri_msgs::IdsList&  ids_list)
+// void handle_hri::tracked_persons_callback(const hri_msgs::IdsList&  ids_list)
+// {
+//   // Debug.
+//   std::cout << "Person User ID -> " << ids_list.ids[0] << std::endl; 
+//   if( !(person_id_.compare(ids_list.ids[0]) == 0) )
+//   {
+//     person_id_ = ids_list.ids[0]; 
+//     handle_hri::matched_id_subscriber_loop();
+//   }
+// }
+
+double handle_hri::set_smile_score()
 {
-  // Debug.
-  std::cout << "Person User ID -> " << ids_list.ids[0] << std::endl; 
-  if( !(person_id_.compare(ids_list.ids[0]) == 0) )
-  {
-    person_id_ = ids_list.ids[0]; 
-    handle_hri::matched_id_subscriber_loop();
-  }
+  return double(smile_acum_/smile_calls_) * 5.0;
 }
