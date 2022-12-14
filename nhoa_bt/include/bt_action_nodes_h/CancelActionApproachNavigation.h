80fff356113b68cf22fe4e8e1d363f3fac9d5c8c
oscar.palacin <oscar.palacin@eurecat.org>
2022-12-14 13:45:06 +0100

    CancelNavigationGoal added.

---
+++ nhoa_bt/include/bt_action_nodes_h/CancelActionApproachNavigation.h
@@ -0,0 +1,58 @@
+#ifndef CancelActionApproachNavigation_H_INCLUDED_
+#define CancelActionApproachNavigation_H_INCLUDED_
+
+// GENERAL INCLUDES
+#include <iostream>
+#include <math.h>
+
+// ROS INCLUDES
+#include <ros/ros.h>
+
+// BT includes.
+#include <behaviortree_cpp_v3/behavior_tree.h>
+#include <behaviortree_cpp_v3/bt_factory.h>
+
+// NHOA_BT INCLUDES
+#include <plan_navigation.h>
+
+
+/* TODO: This BT Action node encapsulates all the functionalities
+         to cancel navigation goal.*/
+
+class CancelActionApproachNavigation : public BT::CoroActionNode
+{
+  public:                                                                                                     
+
+    // Shared program resources.
+    plan_navigation* navigation_;
+
+    // Bool flag.
+    bool      success_              = false;
+
+    // ==============
+
+    CancelActionApproachNavigation(const std::string& name) : 
+    BT::CoroActionNode(name, {})
+    {
+    }
+
+    void init(plan_navigation*       input_navigation)
+    {
+      std::cout << "### Initializing CancelActionApproachNavigation! ###" << std::endl;
+
+      navigation_ = input_navigation;
+    }
+
+    // You must override the virtual function tick()
+    BT::NodeStatus      tick() override;
+    void                cleanup(bool halted);
+    void                halt() override;
+
+    // =================================================== 
+    // Additional functionalities.
+
+    // Cancel Goal navigation goal.
+    bool cancelGoal();
+
+};
+#endif // CancelActionApproachNavigation_H_INCLUDED_
---
+++ nhoa_bt/src/bt_action_nodes/CancelActionApproachNavigation.cpp
@@ -0,0 +1,72 @@
+#include <CancelActionApproachNavigation.h>
+
+// ROS INCLUDES
+#include <ros/ros.h>
+
+// =====================================
+
+BT::NodeStatus CancelActionApproachNavigation::tick()
+{   
+  std::string input;
+  bool waiting = true;
+  bool success = false;
+  while (waiting) 
+  {
+      std::cout << name() << ": Select 's' (start) or 'p'(pause)" << std::endl;
+      std::cin >> input;
+      if (input.compare("s") == 0) 
+      { 
+        if(!(cancelGoal()))
+        {
+            std::cout << "ERROR! Not able to get cancel goal." << std::endl;
+            success = false;
+        }
+        else
+        {
+            std::cout << "SUCCESS! cancel goal." << std::endl;
+            success = true;
+        }
+        waiting = false;
+      } 
+  }
+
+  std::cout << "Asyncronous process finished!"<<std::endl;
+  cleanup(false);
+  if(success)
+  {
+    return BT::NodeStatus::SUCCESS;
+  }
+  else
+  {
+    return BT::NodeStatus::FAILURE;
+  }
+}
+
+
+void CancelActionApproachNavigation::cleanup(bool halted)
+{
+    std::cout << "cleaning up" << std::endl;
+    if(halted)
+    {
+        std::cout << name() <<": cleaning up after an halt()\n" << std::endl;
+    }
+    else{
+        std::cout << name() <<": cleaning up after SUCCESS\n" << std::endl;
+    }
+}
+
+void CancelActionApproachNavigation::halt(){
+
+    std::cout << name() <<": Halted." << std::endl;
+    cleanup(true);
+    // Do not forget to call this at the end.
+    BT::CoroActionNode::halt();
+
+}
+
+// ####################
+// Additional functions.
+bool  CancelActionApproachNavigation::cancelGoal()
+{
+  return navigation_->cancel_goal();
+}
